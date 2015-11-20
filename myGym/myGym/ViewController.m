673e1b2975d2f56847218af418dad9e0f697ff53
coincoin38 <juliengimenez38@gmail.com>
2015-11-20 22:33:43 +0100

    First commit !

---
+++ myGym/myGym/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  myGym
+//
+//  Created by julien gimenez on 20/11/2015.
+//  Copyright © 2015 julien gimenez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ myGym/myGym/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myGym
+//
+//  Created by julien gimenez on 20/11/2015.
+//  Copyright © 2015 julien gimenez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
