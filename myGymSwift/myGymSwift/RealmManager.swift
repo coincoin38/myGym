0f74296fddc60ed3667953816cc50185581ff2a0
Julien Gimenez <jgimenez@sqli.com>
2015-11-30 17:39:10 +0100

    Presque à l'identique de la version objective-c, reste le passage de sessionModel du calendrier à l'écran journée

--- myGymSwift/myGymSwift/RealmManager.swift
+++ myGymSwift/myGymSwift/RealmManager.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 import RealmSwift
+import SwiftyJSON
 
 class RealmManager: NSObject {
 
     // MODELS
-    
     let stub_sessions = 0;
     let stub_teachers = 1;
     
@@ -22,79 +22,107 @@ class RealmManager: NSObject {
     let kSessionsObject = "sessions";
     let kTeachersObject = "teachers";
     
+    let realm = try! Realm()
+    
     static let sharedInstance = RealmManager()
     
+    // MARK: - Ajout d'objets
+    
+    // Remplissage de la base de données
     func feedDataBaseWithFile(key:NSInteger){
         
         returnFileAndObject(key) { (stub, keyStub) -> Void in
             
-            JsonManager.sharedInstance .groupsFromFile(stub, object:keyStub) { (result) -> Void in
-                let realm = try! Realm()
-
+            self.groupsFromFile(stub, object:keyStub) { (result) -> Void in
+                
                 if (key == self.stub_sessions){
-                    for(_,key)in result{
+                    for(_,session)in result{
                         
-                        // Format string to date
-                        let stringDate = key["day"].stringValue
-                        let dateFormatter = NSDateFormatter()
-                        dateFormatter.dateFormat = "yyyy-MM-dd"
-                        let date = dateFormatter.dateFromString(stringDate)
-                        
-                        // Create a session object
-                        let session = SessionModel()
-                        session._id        = key["_id"].stringValue
-                        session.name       = key["name"].stringValue
-                        session.from       = key["from"].stringValue
-                        session.to         = key["to"].stringValue
-                        session.location   = key["location"].stringValue
-                        session.teacher_id = key["teacher_id"].stringValue
-                        session.day        = date!
-                        
+                        let sessionModel = self.generateSession(session)
                         // Add to the Realm inside a transaction
-                        try! realm.write {
-                            realm.add(session)
+                        try! self.realm.write {
+                            self.realm.add(sessionModel)
                         }
                     }
                     // retrieves all Sessions from the default Realm
-                    let allSessions = realm.objects(SessionModel)
-                    print(allSessions)
+                    print(self.getAllSessions())
                 }
                 
                 if (key == self.stub_teachers){
-                    for(_,key)in result{
+                    for(_,teacher)in result{
                         
-                        // Create a teacher object
-                        let teacher = TeacherModel()
-                        teacher._id          = key["_id"].stringValue
-                        teacher.name         = key["name"].stringValue
-                        teacher.first_name   = key["first_name"].stringValue
-                        teacher._description = key["_description"].stringValue
-                        teacher.photo        = key["photo"].stringValue
-                        teacher.agency       = key["agency"].stringValue
-                        
+                        let teacherModel = self.generateTeacher(teacher)
                         // Add to the Realm inside a transaction
-                        try! realm.write {
-                            realm.add(teacher)
+                        try! self.realm.write {
+                            self.realm.add(teacherModel)
                         }
                     }
                     // retrieves all Sessions from the default Realm
-                    let allTeachers = realm.objects(TeacherModel)
-                    print(allTeachers)
+                    print(self.getAllTeachers())
                 }
             }
         }
     }
-    
-    func cleanDb(){
-        
-        // Clean DB
-        let realm = try! Realm()
 
+
+    // Création d'un objet sessionModel
+    func generateSession(dictionary:JSON) ->SessionModel{
+        
+        // Format string to date
+        let stringDate = dictionary["day"].stringValue
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let date = dateFormatter.dateFromString(stringDate)
+
+        let session = SessionModel()
+        session._id        = dictionary["_id"].stringValue
+        session.name       = dictionary["name"].stringValue
+        session.from       = dictionary["from"].stringValue
+        session.to         = dictionary["to"].stringValue
+        session.location   = dictionary["location"].stringValue
+        session.teacher_id = dictionary["teacher_id"].stringValue
+        session.day        = date!
+
+        return session
+    }
+    
+    // Création d'un objet teacherModel
+    func generateTeacher(dictionary:JSON) ->TeacherModel{
+        
+        // Create a teacher object
+        let teacher = TeacherModel()
+        teacher._id          = dictionary["_id"].stringValue
+        teacher.name         = dictionary["name"].stringValue
+        teacher.first_name   = dictionary["first_name"].stringValue
+        teacher._description = dictionary["_description"].stringValue
+        teacher.photo        = dictionary["photo"].stringValue
+        teacher.agency       = dictionary["agency"].stringValue
+        
+        return teacher
+    }
+    
+    // MARK: - Récupération d'objets
+
+    func getAllTeachers()->Results<(TeacherModel)>{
+        return realm.objects(TeacherModel)
+    }
+    
+    func getAllSessions()->Results<(SessionModel)>{
+        return realm.objects(SessionModel)
+    }
+    
+    // MARK: - Suppression de la base de données
+
+    // Reset de la base de données
+    func cleanDb(){
         try! realm.write {
-            realm.deleteAll()
+            self.realm.deleteAll()
         }
     }
     
+    // MARK: - Traitement JSON
+
+    // Quel stub utiliser
     func returnFileAndObject(keyStubDetection: NSInteger,completion:(stub:String,keyStub:String)->Void){
         
         if (keyStubDetection == stub_sessions){
@@ -103,5 +131,14 @@ class RealmManager: NSObject {
         if (keyStubDetection == stub_teachers){
             completion(stub: kTeachersStub,keyStub: kTeachersObject);
         }
+    }
+    
+    // Transformation d'un stub en data JSON
+    func groupsFromFile(fileName: String, object: String, completion:(result:JSON)->Void){
+        
+        let path = NSBundle.mainBundle().pathForResource(fileName, ofType: "json")
+        let dataJson = NSData(contentsOfFile: path!)
+        let json = JSON(data:dataJson!)
+        completion(result: json[object])
     }
 }
--- myGymSwift/myGymSwift/SessionsViewController.swift
+++ myGymSwift/myGymSwift/SessionsViewController.swift
@@ -12,7 +12,8 @@ import FSCalendar
 class SessionsViewController: UIViewController,FSCalendarDelegate,FSCalendarDataSource {
 
     @IBOutlet weak var myCalendar: FSCalendar?
-
+    var selectedDate : NSDate = NSDate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,14 +21,23 @@ class SessionsViewController: UIViewController,FSCalendarDelegate,FSCalendarData
         myCalendar?.delegate = self
         myCalendar?.dataSource = self
         myCalendar?.appearance.headerMinimumDissolvedAlpha = 0.0;
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: - FSCalendar delegate
 
-    /*
+    func calendar(calendar: FSCalendar!, didSelectDate date: NSDate!) {
+       
+        selectedDate = date
+        self.performSegueWithIdentifier("showDetailDay", sender: self)
+    }
+
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -35,6 +45,5 @@ class SessionsViewController: UIViewController,FSCalendarDelegate,FSCalendarData
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
 
 }
