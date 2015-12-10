d3a4c347434f589b2c2c1a00a0fff9d9a7da6f45
Julien Gimenez <jgimenez@sqli.com>
2015-12-10 11:18:10 +0100

    Préparation WeekManager

---
+++ myGymSwift/myGymSwift/WeekManager.swift
@@ -0,0 +1,13 @@
+//
+//  WeekManager.swift
+//  myGymSwift
+//
+//  Created by SQLI51109 on 10/12/2015.
+//  Copyright © 2015 julien gimenez. All rights reserved.
+//
+
+import UIKit
+
+class WeekManager: NSObject {
+
+}
