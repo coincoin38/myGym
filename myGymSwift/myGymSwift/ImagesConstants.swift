8783409d41af33abcbead37d9c079488feb4ff92
Julien Gimenez <jgimenez@sqli.com>
2016-01-27 18:03:37 +0100

    Mis en place écran contact / plan

--- myGymSwift/myGymSwift/ImagesConstants.swift
+++ myGymSwift/myGymSwift/ImagesConstants.swift
@@ -11,9 +11,11 @@ struct ImagesConstants {
     static let tabBarSelectedImage0 = "News_selected_V2"
     static let tabBarSelectedImage1 = "Sessions_selected"
     static let tabBarSelectedImage2 = "Sports_selected_V2"
-    
+    static let tabBarSelectedImage3 = "Contacts_selected"
+
     static let tabBarDefaultImage0 = "News_unselected_V2"
     static let tabBarDefaultImage1 = "Sessions"
     static let tabBarDefaultImage2 = "Sports_unselected"
+    static let tabBarDefaultImage3 = "Contacts_unselected"
 
 }
--- myGymSwift/myGymSwift/MainTabBarViewController.swift
+++ myGymSwift/myGymSwift/MainTabBarViewController.swift
@@ -20,19 +20,23 @@ class MainTabBarViewController: UITabBarController {
         let firstTabBarItem:UITabBarItem  = tabBar.items![0]
         let secondTabBarItem:UITabBarItem = tabBar.items![1]
         let thirdTabBarItem:UITabBarItem  = tabBar.items![2]
-        
+        let fourthTabBarItem:UITabBarItem = tabBar.items![3]
+
         firstTabBarItem.selectedImage  = UIImage(named: ImagesConstants.tabBarSelectedImage0)
         secondTabBarItem.selectedImage = UIImage(named: ImagesConstants.tabBarSelectedImage1)
         thirdTabBarItem.selectedImage  = UIImage(named: ImagesConstants.tabBarSelectedImage2)
+        fourthTabBarItem.selectedImage  = UIImage(named: ImagesConstants.tabBarSelectedImage3)
 
         firstTabBarItem.image  = UIImage(named: ImagesConstants.tabBarDefaultImage0)
         secondTabBarItem.image = UIImage(named: ImagesConstants.tabBarDefaultImage1)
         thirdTabBarItem.image  = UIImage(named: ImagesConstants.tabBarDefaultImage2)
-        
+        fourthTabBarItem.image  = UIImage(named: ImagesConstants.tabBarDefaultImage3)
+
         firstTabBarItem.setTitleTextAttributes([NSFontAttributeName:UIFont.systemFontOfSize(13, weight: 2)], forState: .Normal)
         secondTabBarItem.setTitleTextAttributes([NSFontAttributeName:UIFont.systemFontOfSize(13, weight: 2)], forState: .Normal)
         thirdTabBarItem.setTitleTextAttributes([NSFontAttributeName:UIFont.systemFontOfSize(13, weight: 2)], forState: .Normal)
-        
+        fourthTabBarItem.setTitleTextAttributes([NSFontAttributeName:UIFont.systemFontOfSize(13, weight: 2)], forState: .Normal)
+
     }
 
     override func didReceiveMemoryWarning() {
